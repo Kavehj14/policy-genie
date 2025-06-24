5d7b3c3f84004a64c30c686d341d0a807d34d589
Kavehj14 <143360495+Kavehj14@users.noreply.github.com>
2025-06-24 18:08:18 +0100

    Fix Vite root configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './project-bolt-sb1-5rakyefp/project',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
